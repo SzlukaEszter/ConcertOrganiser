630bdf81c9a0e19e1e546b4a0ae1983205bf3093
Eszter Szluka <szluka.eszter@gmail.com>
2019-10-26 15:10:27 +0200

    'add FestivalConcert'

---
+++ src/com/codecool/SzlukaEszter/concerts/FestivalConcert.java
@@ -0,0 +1,16 @@
+package com.codecool.SzlukaEszter.concerts;
+
+import com.codecool.SzlukaEszter.bands.MainBand;
+
+public class FestivalConcert extends Concert {
+
+    public FestivalConcert(MainBand mainBand) {
+        super(mainBand);
+        capacity = 8000;
+        duration = 6;
+        ticketPrice = getTicketPrice();
+        beerPrice = 1000;
+        //todo: style =
+    }
+
+}
