a41051771700f1b70e6719f2153d2610372cc585
Eszter Szluka <szluka.eszter@gmail.com>
2019-10-26 15:23:51 +0200

    'correct constructor WarmUpBand'

--- src/com/codecool/SzlukaEszter/bands/WarmUpBand.java
+++ src/com/codecool/SzlukaEszter/bands/WarmUpBand.java
@@ -2,7 +2,7 @@ package com.codecool.SzlukaEszter.bands;
 
 public class WarmUpBand extends Band{
 
-    public WarmUpBand(String name) {
-        super(name);
+    public WarmUpBand(String name, Style style) {
+        super(name, style);
     }
 }
